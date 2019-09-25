59fce137ac71eca294e8931fb088da252dafe4f8
Tom Metcalfe <t.metcalfe@rasa.com>
2019-09-25 11:16:55 +0100

    Add exporting functions, update main

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import argparse
-from src.loading import load_timestamps
+from src.io import load_events, export_averages
+from src.processing import moving_averages
 
 
 def create_arg_parser() -> argparse.ArgumentParser:
     """Parse the command line arguments for the unbabel_cli script."""
 
     parser = argparse.ArgumentParser(
-        prog="Unbabel CLI",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        prog="Unbabel CLI", formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
     parser.add_argument(
-        "-i",
-        "--input_file",
-        required=True,
-        help="Path to the events JSON file",
+        "-i", "--input_file", required=True, help="Path to the events JSON file"
     )
 
     parser.add_argument(
@@ -32,4 +29,6 @@ if __name__ == "__main__":
     parser = create_arg_parser()
     args = parser.parse_args()
 
-    data = load_timestamps(args.input_file)
+    events = load_events(args.input_file)
+    averages = moving_averages(events, args.window_size)
+    export_averages(averages, "output.json")
---
+++ src/io.py
@@ -0,0 +1,88 @@
+import json
+from typing import Text, List, Dict, Any
+from pandas import DataFrame
+from datetime import datetime
+import logging
+
+INPUT_TIMESTAMP_STR = "%Y-%m-%d %H:%M:%S.%f"
+OUTPUT_TIMESTAMP_STR = "%Y-%m-%d %H:%M:%S"
+
+
+class UnknownFormatError(BaseException):
+    """Error raised when input file is not JSON standard."""
+
+    def __init__(self, filepath: Text, error: json.JSONDecodeError):
+        self.filepath = filepath
+        self.error = error
+
+    def __str__(self):
+        message = (
+            "Failed to load file '{}' because it is not in valid JSON format."
+            "More info: {}"
+        )
+        return message.format(self.filepath, self.error)
+
+
+def load_events(filepath: Text) -> DataFrame:
+    """Loads event data from a JSON file."""
+
+    try:
+        events = _read_json(filepath)
+    except json.JSONDecodeError as e:
+        raise UnknownFormatError(filepath, e)
+
+    events_df = _build_dataframe(events)
+    logging.info("Loaded {} events from '{}".format(len(events_df), filepath))
+
+    return events_df
+
+
+def _read_json(filepath: Text) -> List[Dict[Text, Any]]:
+    """Reads lines from JSON and converts timestamps to datetimes."""
+    events = []
+
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            event = json.loads(line)
+            event["timestamp"] = datetime.strptime(
+                event["timestamp"], INPUT_TIMESTAMP_STR
+            )
+            events.append(event)
+
+    return events
+
+
+def _build_dataframe(events: List[Dict[Text, Any]]) -> DataFrame:
+    """Converts timestamps to datetimes and returns a DataFrame."""
+
+    events_df = DataFrame(data=events)
+    events_df["timestamp"] = events_df["timestamp"].astype(datetime)
+
+    return events_df
+
+
+def export_averages(averages: List[Dict[Text, Any]], filepath: Text):
+    """Write averages to file in the format we expect."""
+
+    filestr = _format_file_string(averages)
+
+    with open(filepath, "w+") as f:
+        f.write(filestr)
+
+    logging.info("Dumped {} averages to '{}".format(len(averages), filepath))
+
+
+def _format_file_string(averages: List[Dict[Text, Any]]) -> Text:
+    """Converts datetimes to strings and removes floating zeroes."""
+
+    filestr = ""
+
+    for avg in averages:
+        avg["date"] = datetime.strftime(avg["date"], OUTPUT_TIMESTAMP_STR)
+
+        if avg["average_delivery_time"] % 1 == 0:
+            avg["average_delivery_time"] = int(avg["average_delivery_time"])
+
+        filestr += json.dumps(avg) + "\n"
+
+    return filestr.rstrip("\n")
