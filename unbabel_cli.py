4b257b2a17b4111c7a437b362d0967891373a4b6
Kecia <=>
2019-10-06 16:54:23 -0600

    added methods

--- unbabel_cli.py
+++ unbabel_cli.py
@@ -12,47 +12,51 @@ def check_int(value):
 		raise argparse.ArgumentTypeError("%s should be a integer a grater than 0" % value)
 	return ivalue
 
-
 ### get command line args
-parser = argparse.ArgumentParser(description='Aggregate events by minutes')
-parser.add_argument('--input_file',  required=True, type=str, help='Json file name with events')
-parser.add_argument('--window_size',  required=True, type=check_int, help='Moving avg window size (in min)')
-args = parser.parse_args()
-print(args.window_size)
+def get_command_args():
+	parser = argparse.ArgumentParser(description='Aggregate events by minutes')
+	parser.add_argument('--input_file',  required=True, type=str, help='Json file name with events')
+	parser.add_argument('--window_size',  required=True, type=check_int, help='Moving avg window size (in min)')
+	args = parser.parse_args()
+	return args
 
-
-input_file = args.input_file
-window_size = args.window_size
 ### read input data
-data = []
-with open(input_file) as f:
-	for line in f:
-		data.append(json.loads(line))
-f.close()
-
+def read_input_file(input_file: str) -> list:
+	data = []
+	with open(input_file) as f:
+		for line in f:
+			data.append(json.loads(line))
+	f.close()
+	return data
 
 ### calc avg for every min according to window size
-
-#### transform in dataframe
-df = json_normalize(data)
-#### convert to timestamp
-df["timestamp"]  = pd.to_datetime(df["timestamp"],format='%Y-%m-%d %H:%M:%S')
-#### define the start and end data
-ref = df["timestamp"].min().floor(freq = 'T')
-end = df["timestamp"].max().ceil(freq = 'T')
-#### generate data
-result = []
-while ref <= end:
-	past = ref - timedelta(minutes = window_size)
-	df_window = df[(df["timestamp"] <= ref) & (df["timestamp"] >= past)]
-	avg = df_window["duration"].mean() if len(df_window) > 0 else 0
-	result.append({"date":str(ref), "average_delivery_time":avg})
-	ref += timedelta(minutes = 1)
-
-
-print(result)
+def cacl_avg_delivery_time(data: pd.DataFrame, window_size: int) -> list:
+	#### transform in dataframe
+	df = json_normalize(data)
+	#### convert to timestamp
+	df["timestamp"]  = pd.to_datetime(df["timestamp"],format='%Y-%m-%d %H:%M:%S')
+	#### define the start and end data
+	ref = df["timestamp"].min().floor(freq = 'T')
+	end = df["timestamp"].max().ceil(freq = 'T')
+	#### generate data
+	result = []
+	while ref <= end:
+		past = ref - timedelta(minutes = window_size)
+		df_window = df[(df["timestamp"] <= ref) & (df["timestamp"] >= past)]
+		avg = df_window["duration"].mean() if len(df_window) > 0 else 0
+		result.append({"date":str(ref), "average_delivery_time":avg})
+		ref += timedelta(minutes = 1)
+	return result
 
 ### write output data
-with open('outputfile.json', 'w') as f:
-	for r in result:
-		f.write("%s\n" % json.dumps(r))
+def write_output_file(result:dict):
+	with open('outputfile.json', 'w') as f:
+		for r in result:
+			f.write("%s\n" % json.dumps(r))
+
+if __name__ == '__main__':
+	args = get_command_args()
+	data = read_input_file(args.input_file)
+	print(args)
+	result = cacl_avg_delivery_time(data, args.window_size)
+	write_output_file(result)
