da4994108a48b2d3d3921011467dd14c12ff268f
Kecia <=>
2019-10-06 17:06:17 -0600

    handle execptions

--- unbabel_cli.py
+++ unbabel_cli.py
@@ -4,6 +4,7 @@ import pandas as pd
 from pandas.io.json import json_normalize
 from datetime import datetime, timedelta
 import argparse
+import sys
 
 ## check if window_size is a valid integer
 def check_int(value):
@@ -23,36 +24,49 @@ def get_command_args():
 ### read input data
 def read_input_file(input_file: str) -> list:
 	data = []
-	with open(input_file) as f:
-		for line in f:
-			data.append(json.loads(line))
-	f.close()
+	try:
+		with open(input_file) as f:
+			for line in f:
+				data.append(json.loads(line))
+			f.close()
+	except:
+		print("Error when reading file! Check if file exits and format is ok!") 
+		sys.exit(1)
+
 	return data
 
 ### calc avg for every min according to window size
 def cacl_avg_delivery_time(data: pd.DataFrame, window_size: int) -> list:
 	#### transform in dataframe
 	df = json_normalize(data)
-	#### convert to timestamp
-	df["timestamp"]  = pd.to_datetime(df["timestamp"],format='%Y-%m-%d %H:%M:%S')
-	#### define the start and end data
-	ref = df["timestamp"].min().floor(freq = 'T')
-	end = df["timestamp"].max().ceil(freq = 'T')
-	#### generate data
-	result = []
-	while ref <= end:
-		past = ref - timedelta(minutes = window_size)
-		df_window = df[(df["timestamp"] <= ref) & (df["timestamp"] >= past)]
-		avg = df_window["duration"].mean() if len(df_window) > 0 else 0
-		result.append({"date":str(ref), "average_delivery_time":avg})
-		ref += timedelta(minutes = 1)
-	return result
+	try:
+		#### convert to timestamp
+		df["timestamp"]  = pd.to_datetime(df["timestamp"],format='%Y-%m-%d %H:%M:%S')
+		#### define the start and end data
+		ref = df["timestamp"].min().floor(freq = 'T')
+		end = df["timestamp"].max().ceil(freq = 'T')
+		#### generate data
+		result = []
+		while ref <= end:
+			past = ref - timedelta(minutes = window_size)
+			df_window = df[(df["timestamp"] <= ref) & (df["timestamp"] >= past)]
+			avg = df_window["duration"].mean() if len(df_window) > 0 else 0
+			result.append({"date":str(ref), "average_delivery_time":avg})
+			ref += timedelta(minutes = 1)
+		return result
+	except:
+		print("Oops! Apparently data in input_file are not as expected!")
+		sys.exit(2)    
 
 ### write output data
 def write_output_file(result:dict):
-	with open('outputfile.json', 'w') as f:
-		for r in result:
-			f.write("%s\n" % json.dumps(r))
+	try:
+		with open('outputfile.json', 'w') as f:
+			for r in result:
+				f.write("%s\n" % json.dumps(r))
+	except:
+		print("Error when writing to file") 
+		sys.exit(3) 
 
 if __name__ == '__main__':
 	args = get_command_args()
