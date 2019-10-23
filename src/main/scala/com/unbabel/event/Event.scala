21b001b8ba9639acb933a07ead2b4cafd671098b
Claudia Henriques <claudia.henriques@xpand-it.com>
2019-10-23 23:32:30 +0100

    Requirements and equals added to the Event class

--- src/main/scala/com/unbabel/event/Event.scala
+++ src/main/scala/com/unbabel/event/Event.scala
@@ -28,7 +28,37 @@ case class Event(
                   @JsonProperty("nr_words") nr_words: Long
                 ) {
 
-  val formatter = DateTimeFormatter.ofPattern(INPUTDATEFORMAT)
-  val timestampDateTime = LocalDateTime.parse(timestamp, formatter)
+  // Requires that STRING fields are not null or empty
+  require(timestamp != null && !timestamp.isEmpty, "The event field TIMESTAMP must not be null or empty")
+  require(translation_id != null && !translation_id.isEmpty, "The event field TRANSLATION_ID must not be null or empty")
+  require(lang_src != null && !lang_src.isEmpty, "The event field SOURCE_LANGUAGE must not be null or empty")
+  require(lang_trg != null && !lang_trg.isEmpty, "The event field TARGET_LANGUAGE must not be null or empty")
+  require(client != null && !client.isEmpty, "The event field CLIENT_NAME must not be null or empty")
+  require(name != null && !name.isEmpty, "The event field EVENT_NAME must not be null or empty")
 
+  // Requires that LONG fields are non-negative
+  require(nr_words >= 0 && duration >= 0, "The event fields NR_WORDS and DURATION must be non-negative")
+
+  // LocalDateTime based on timestamp
+  private val formatter : DateTimeFormatter = DateTimeFormatter.ofPattern(INPUTDATEFORMAT)
+  val timestampDateTime : LocalDateTime = LocalDateTime.parse(timestamp, formatter)
+
+  def canEqual(that: Any) : Boolean = that.isInstanceOf[Event]
+
+  override def equals(that: Any) : Boolean = {
+    that match {
+      case that: Event =>
+        that.canEqual(this) &&
+          this.timestamp.equals(timestamp) &&
+          this.translation_id.equals(that.translation_id) &&
+          this.lang_src.equals(that.lang_src) &&
+          this.lang_trg.equals(that.lang_trg) &&
+          this.client.equals(that.client) &&
+          this.name.equals(that.name) &&
+          this.duration == that.duration &&
+          this.nr_words == that.nr_words
+
+      case _ => false
+    }
+  }
 }
