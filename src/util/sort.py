69468f19e3c764a2b9b7df7500812f7e8497c5e5
Agustin Oliver <agustinoliver1998@gmail.com>
2022-06-29 14:48:51 -0300

    utility for sort

---
+++ src/util/sort.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+date_format = '%Y-%m-%d %H:%M:%S.%f'
+
+
+def sortEventList(events):
+    return sorted(events, key=lambda x: datetime.strptime(x['timestamp'], date_format))
