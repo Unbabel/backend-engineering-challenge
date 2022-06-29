f5d8cde49e255d9797ba3372dceee4389b0ad87b
Agustin Oliver <agustinoliver1998@gmail.com>
2022-06-29 11:30:12 -0300

    working on processor.py with pandas

--- src/processor.py
+++ src/processor.py
@@ -1,4 +1,6 @@
 import datetime
+
+import numpy as np
 import pandas as pd
 
 
@@ -6,22 +8,21 @@ def process(df, window_size) -> pd.DataFrame:
     return _calculateAvg(df, window_size)
 
 
+# For every window of x minutes will create a dataframe of [dates,duration]
+# Will filter the selected records for corresponding window [dates,duration]
+# calculates the moving average with rolling function [dates, avg]
+# return a master dataframe of the output
 def _calculateAvg(df, window_size) -> pd.DataFrame:
     df['dates'] = df['timestamp'].astype('datetime64[s]')
     df2 = df[['dates', 'duration']]
-    start_time = df['dates'].min()
-    running_time = start_time
-    firstRun = True
-    end_time = df['dates'].max()
-    while start_time < end_time + datetime.timedelta(minutes=1):
-        dummy_df = pd.DataFrame(pd.date_range(start=start_time,
-                                              end=start_time + datetime.timedelta(minutes=window_size), freq='min'),
-                                columns=['dates'])
-        print(dummy_df)
-        start_time += datetime.timedelta(minutes=window_size)
-# output_frame.loc[output_frame.dates.between(df.dates), 'avg'] = df.duration
-# df.resample("1m").describe.med()
-
+    start_time = df2['dates'].dt.round('min').min()
+    dummy_df = pd.DataFrame(pd.date_range(start=start_time,
+                                          end=start_time + datetime.timedelta(minutes=window_size), freq='min'),
+                            columns=['dates'])
+    dummy_df['duration'] = 0
+    selected_records = df2[df2['dates'].between(start_time, start_time + datetime.timedelta(minutes=window_size))]
+    merged_df = pd.concat([dummy_df, selected_records])
+    merged_df.resample
 #
 #   2018-12-26 18:12:00, 0.0
 #   2018-12-26 18:12:07, 20.0
