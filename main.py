c936b6ac6fb3645e6ffef10ab722daa1aecb08ed
Agustin Oliver <agustinoliver1998@gmail.com>
2022-06-29 16:29:44 -0300

    adding type of parameter

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ from src.processor.processor import process
 from src.writer.writer import write
 import argparse
 
-
-# Press the green button in the gutter to run the script.
-# For each iteration I'll have the current timestamp - window size
-# every minute will compare:
-# 1. if no data available avg_delivered_time = 0
-# 2. if data is available I have to track how many translation already happened and sum the time of those translations
-# to get the avg
-# 3. handle edge cases
-# 4. create output
-# 4. unit tests
-
 def read_arguments():
     parser = argparse.ArgumentParser(description='Events Aggregated per minute')
     parser.add_argument('--input_file', required=True, type=str, help='Input file')
@@ -23,11 +12,7 @@ def read_arguments():
 
 
 if __name__ == '__main__':
-    # args = read_arguments()
-    args = {
-        "input_file": 'files/events.json',
-        "window_size": 10
-    }
-    df = read(args['input_file'])
-    out_frame = process(df, args['window_size'])
+    args = read_arguments()
+    df = read(args.input_file)
+    out_frame = process(df, args.window_size)
     write(out_frame)
