ce0379eaab3c7083b8a0be73a6bdd830be36215c
Agustin Oliver <agustinoliver1998@gmail.com>
2022-06-30 17:50:17 -0300

    read args

--- main.py
+++ main.py
@@ -8,16 +8,13 @@ def read_arguments():
     parser = argparse.ArgumentParser(description='Events Aggregated per minute')
     parser.add_argument('--input_file', required=True, type=str, help='Input file')
     parser.add_argument('--window_size', required=True, type=int, help='Window Size')
+    parser.add_argument('--output_file', required=True, type=str, help='Output file')
     args = parser.parse_args()
     return args
 
 
 if __name__ == '__main__':
-    # args = read_arguments()
-    args = {
-        "input_file": 'files/events.json',
-        "window_size": 10
-    }
-    events = read(args['input_file'])
-    moving_average_list = process(events, args['window_size'])
-    write(moving_average_list, 'files/output.json')
+    args = read_arguments()
+    events = read(args.input_file)
+    moving_average_list = process(events, args.window_size)
+    write(moving_average_list, args.output_file)
