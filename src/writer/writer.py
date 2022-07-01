2913302508fe755411c3b86e05e66677f4dede9c
Agustin Oliver <agustinoliver1998@gmail.com>
2022-07-01 12:51:22 -0300

    rename columns

--- src/writer/writer.py
+++ src/writer/writer.py
@@ -3,4 +3,5 @@ import pandas as pd
 
 def write(output_frame: pd.DataFrame):
     print(output_frame)
+    output_frame = output_frame.rename(columns={'dates': 'date', 'duration': 'average_delivered_time'})
     output_frame.to_json('files/output.json', orient='records', date_format='iso', lines=True)
