09c07f7e90ffd101e1e9a9071781c3b14eedff06
thisIsMailson <mailsonteixeiravarela@gmail.com>
2024-01-29 21:40:19 +0000

    added debug prints

--- main.py
+++ main.py
@@ -15,23 +15,29 @@ def calculate_moving_average(input_file, window_size):
         event_queue.append((timestamp, duration))
         while event_queue and timestamp - event_queue[0][0] > timedelta(minutes=window_size):
             event_queue.pop(0)
-        
+        # print(f"After while loop - event_queue: {event_queue}")
         # Calculate moving averages for each minute within the current time window
         current_time = timestamp
         window_start_time = current_time - timedelta(minutes=window_size)
 
         while event_queue and event_queue[0][0] < current_time:
-            moving_average = round(sum(duration for _, duration in event_queue) / len(event_queue), 2)
+            # Filter events within the current time window [current_minute - window_size, current_minute]
+            events_within_window = [(time, duration) for time, duration in event_queue if window_start_time <= time <= current_time]
+            print('them evts', events_within_window)
+            # Calculate moving average only if there are events within the window
+            if events_within_window:
+                moving_average = round(sum(duration for _, duration in events_within_window) / len(events_within_window), 2)
+            else:
+                moving_average = 0
+
             average_delivery_times.append({"date": current_time.strftime('%Y-%m-%d %H:%M:%S'), "average_delivery_time": moving_average})
 
             # Move the time window to the next minute
             current_time -= timedelta(minutes=1)
-        # print(average_delivery_times)
-        # if event_queue:
-        #     moving_average = sum(duration for _, duration in event_queue) / len(event_queue)
-        #     print(moving_average)
-        #     average_delivery_times.append({'date': timestamp.strftime('%Y-%m-%d %H:%M:%S'), 'average_delivery_time': moving_average})
+            window_start_time = current_time - timedelta(minutes=window_size)  # Update the window start time
 
+        # print(average_delivery_times)
+        
     save_to_file(average_time=average_delivery_times)
 
 def save_to_file(average_time):
